949f594c6a89851cdbfabc61a37ea5600ed2e43a
Garrett Brown <gdbrown09@gmail.com>
2021-06-19 19:07:21 -0500

    fixing import

--- src/feature-request/feature-request.module.ts
+++ src/feature-request/feature-request.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { FeatureRequestService } from './feature-request.service';
 import { FeatureRequestController } from './feature-request.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { FeatureRequest } from 'src/schemas/featureRequest.schema';
+import { FeatureRequest } from '../schemas/featureRequest.schema';
 
 @Module({
   imports: [MongooseModule.forFeature([{ name: 'FeatureRequest', schema: FeatureRequest }], 'featureRequests')],
